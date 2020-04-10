c7eacb1abddd342400ab57db2e9c9828c39e8051
f.yoshino <f.yoshino@roseaupensant.jp>
2020-04-10 12:42:00 +0900

    昨日作ったタブページを単一ファイルコンポーネントを使ったものに書き換えた
    Vue CLIでプロジェクトを作成した

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <tab-view></tab-view>
+  </div>
+</template>
+
+<script>
+import TabView from './components/TabView.vue'
+import Tab from './components/Tab.vue'
+
+export default {
+  // name: 'App',
+  components: {
+    'tab-view': TabView,
+    'tab-button': Tab
+  }
+}
+</script>
---
+++ src/components/TabView.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="example">
+    <div class="tabs">
+      <TabItem
+        v-for="item in list"
+        v-bind="item" :key="item.id"
+        v-model="currentId"/>
+    </div>
+    <div class="contents">
+      <transition>
+        <section class="item" :key="currentId">
+          {{ current.content }}
+        </section>
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script>
+  import TabButton from './Tab.vue'
+  export default {
+    components: {
+      'TabItem': TabButton
+    },
+    data() {
+      return {
+        currentId: 1,
+        list: [
+          { id: 1, label: 'Tab1', content: 'コンテンツ1' },
+          { id: 2, label: 'Tab2', content: 'コンテンツ2' },
+          { id: 3, label: 'Tab3', content: 'コンテンツ3' }
+        ]
+      }
+    },
+    computed: {
+      current() {
+        return this.list.find(elll => elll.id === this.currentId) || {}
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .contents {
+  position: relative;
+  overflow: hidden;
+  width: 280px;
+  border: 2px solid #000;
+  }
+  .item {
+    box-sizing: border-box;
+    padding: 10px;
+    width: 100%;
+    transition: all 0.8s ease;
+  }
+  /* トランジション用スタイル */
+  .v-leave-active {
+    position: absolute;
+  }
+  .v-enter {
+    transform: translateX(-100%);
+  }
+  .v-leave-to {
+    transform: translateX(100%);
+  }
+</style>
